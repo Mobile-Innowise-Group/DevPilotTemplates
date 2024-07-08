53d75e9d607a5ad0208e664a4821efded9b814d9
German Perelmuter <german.perelmuter@innowise.com>
2024-07-08 11:06:21 +0200

    Refactored DI to follow the same codestyle across the app, split DI into modules, fixed script was deleting temporary directory

--- core/lib/di/app_di.dart
+++ core/lib/di/app_di.dart
@@ -2,15 +2,14 @@ import 'package:get_it/get_it.dart';
 
 import '../config/app_config.dart';
 
-final AppDI appDI = AppDI();
 final GetIt appLocator = GetIt.instance;
 
 const String unauthScope = 'unauthScope';
 const String authScope = 'authScope';
 
-class AppDI {
-  static void initDependencies(Flavor flavor) {
-    appLocator.registerSingleton<AppConfig>(
+abstract class AppDI {
+  static void initDependencies(GetIt locator, Flavor flavor) {
+    locator.registerSingleton<AppConfig>(
       AppConfig.fromFlavor(flavor),
     );
   }
--- domain/lib/domain.dart
+++ domain/lib/domain.dart
@@ -1,5 +1,6 @@
 library domain;
 
+export 'di/domain_di.dart';
 export 'error_handler/app_exception.dart';
 export 'models/models.dart';
 export 'repositories/repositories.dart';
