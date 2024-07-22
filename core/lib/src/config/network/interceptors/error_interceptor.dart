c4870d76f29793590d05e8dd02e2a2a4a904fb16
German Perelmuter <german.perelmuter@innowise.com>
2024-07-22 14:54:10 +0200

    Adjusted dio error interceptor

--- core/lib/src/config/network/interceptors/error_interceptor.dart
+++ core/lib/src/config/network/interceptors/error_interceptor.dart
@@ -10,48 +10,8 @@ class ErrorInterceptor extends Interceptor {
     DioException err,
     ErrorInterceptorHandler handler,
   ) async {
-    switch (err.type) {
-      case DioExceptionType.cancel:
-        break;
-      case DioExceptionType.connectionTimeout:
-        break;
-      case DioExceptionType.receiveTimeout:
-        break;
-      case DioExceptionType.sendTimeout:
-        break;
-      case DioExceptionType.badResponse:
-        switch (err.response?.statusCode) {
-          case 400:
-            break;
-          case 404:
-            break;
-          case 500:
-            break;
-        }
-        break;
-      case DioExceptionType.connectionError:
-        // TODO: Handle this case.
-        break;
-      case DioExceptionType.badCertificate:
-        // TODO: Handle this case.
-        break;
-      case DioExceptionType.unknown:
-        // TODO: Handle this case.
-        break;
-    }
-
-    if (err.response?.statusCode != 401) {
-      return handler.next(err);
-    }
-    final Response<dynamic> response =
-        err.response ?? Response<dynamic>(requestOptions: err.requestOptions);
-    return handler.resolve(response);
-  }
-
-  Future<void> handle401Error(
-    DioException error,
-    ErrorInterceptorHandler handler,
-  ) async {
-    return handler.next(error);
+    return handler.resolve(
+      err.response ?? Response<dynamic>(requestOptions: err.requestOptions),
+    );
   }
 }
