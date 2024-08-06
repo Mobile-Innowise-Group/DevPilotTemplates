9342dd14496c8eb7832a2ffff4e05059cd0181ff
German Perelmuter <german.perelmuter@innowise.com>
2024-08-06 15:43:08 +0200

    Added missing imports

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -10,6 +10,6 @@ export 'src/config/network/dio_config.dart';
 export 'src/constants/app_constants.dart';
 export 'src/di/app_di.dart';
 export 'src/events/events.dart';
-export 'src/localization/app_localization.dart';
+export 'src/localization/localization.dart';
 export 'src/services/services.dart';
 export 'src/utils/extensions.dart';
