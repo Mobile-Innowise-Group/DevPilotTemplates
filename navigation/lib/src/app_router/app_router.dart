c530819c8adab9f2992faef92b486f1d82b880b2
German Perelmuter <german.perelmuter@innowise.com>
2024-08-22 13:16:13 +0200

    Adjusted app router setup

--- navigation/lib/src/app_router/app_router.dart
+++ navigation/lib/src/app_router/app_router.dart
@@ -2,7 +2,9 @@ import 'package:auto_route/auto_route.dart';
 
 part 'app_router.gr.dart';
 
-@AutoRouterConfig(replaceInRouteName: 'Screen,Route')
+@AutoRouterConfig(
+  modules: <Type>[],
+)
 class AppRouter extends _$AppRouter {
   @override
   List<AutoRoute> get routes => <AutoRoute>[
