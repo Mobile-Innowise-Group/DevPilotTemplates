62a7125eeda90546e2118e9b86778a411e3c53b0
GermanPerelmuter <german.perelmuter@innowise.com>
2025-04-01 14:49:51 +0200

    Minor warning fix

--- core_ui/lib/src/theme/colors/app_colors_theme.dart
+++ core_ui/lib/src/theme/colors/app_colors_theme.dart
@@ -1,5 +1,6 @@
-import 'package:core_ui/core_ui.dart';
 import 'package:flutter/material.dart';
+
+import '../../../core_ui.dart';
 
 abstract class AppColorsTheme extends ThemeExtension<AppColorsTheme> {
   const AppColorsTheme();
